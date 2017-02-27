0f8db7cb4ccec2591c4056706bc81b59853356b2
osst1652 <oskar.steinhauf@gmail.com>
2017-02-27 19:01:57 +0100

    Lagt till grundläggande delar
    Lagt till de variabler som behövs samt de funktioner som ska vara med.
    
    Dessutom nodklassen längst ner.

--- src/alda/skip_list/MySkipList.java
+++ src/alda/skip_list/MySkipList.java
@@ -1,5 +1,110 @@
 package alda.skip_list;
 
-public class MySkipList {
+import java.util.Random;
 
+
+/**
+ * ALDA IN8 - uppgift ur boken
+ * 
+ * Write the routines to perform insertion, deletion, and searching in skip lists.
+ * 
+ * 
+ * 
+ * @author Oskar Steinhauf
+ * 
+ * */
+
+
+public class MySkipList<K extends Comparable<K>, V>{
+	
+	private SkipNode head;
+	private Random random;
+	private long siz;
+	private double probabilityFactor;
+	
+	
+	/**
+	 * Konstruktor av skip list.
+	 * */
+	public MySkipList() {
+		head = new SkipNode (null, null, null, null, 0);
+		random = new Random();
+		siz = 0;
+		probabilityFactor = 0.5;
+		
+	}
+	
+	/**
+	 * Insert an element in the skip list.
+	 * 
+	 * @param key
+	 * 
+	 * @param value
+	 * 
+	 *
+	 * */
+	public void Insert(K key, V value){
+		
+	}
+	
+	
+	/**
+	 * Delete an element in the skip list.
+	 * 
+	 * @param Key of which this element in the list will be removed
+	 * 
+	 * */
+	public V delete(K key){
+		V value = null;
+		
+		return value;
+	}
+	
+	
+	/**
+	 * Searching for an element in the skip List
+	 * 
+	 *
+	 * @param key
+	 * 
+	 * */
+	
+	public V get(K key){
+		
+		return null;
+	}
+	
+
+	/**
+	 * All the private functions and methods that are used.
+
+	 * */
+
+	private long level(){
+		long level = 0;
+		do{
+			level++;
+		}while(level <= siz && random.nextDouble() < probabilityFactor);
+		return level;
+	}
+
+	private class SkipNode{
+		public K key;
+		public V value;
+		
+		public SkipNode next;
+		public SkipNode down;
+		
+		public long level;
+	
+		public SkipNode(K key, V value, SkipNode next, SkipNode Down, long level){
+			this.key = key;
+			this.value = value;
+			this.next = next;
+			this.down = down;
+			this.level = level;
+		}
+	
+	}
 }
+
---
+++ src/alda/skip_list/SkipListInterface.java
@@ -0,0 +1,2 @@
+package alda.skip_list;
+
